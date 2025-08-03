394cc5a162ccaa9953bf1baece503fc4392c6a7c
Matt-d-schmidt <12345689+05schmidtm18@gmail.com.github.com>
2025-08-03 12:37:12 -0400

    adding the styling for the grid and title

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ for (let i = 0; i < gridItems; i++) {
     div.classList.add('grid-item');
     container.appendChild(div);
 }
+
+const gridItemStyle = `
+    .grid-item {
+        width: calc(100% / ${gridSize});
+        height: calc(100% / ${gridSize});
+        background-color: lightgray;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+`;
+
+const styleSheet = document.createElement("style");
+styleSheet.type = "text/css";
+styleSheet.innerText = gridItemStyle;
+document.head.appendChild(styleSheet);
